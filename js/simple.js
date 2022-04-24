b4f345ec41de1d0b5d4aea8b81f2d9e435bb891b
Oscar Uribe <aenemapusher24@gmail.com>
2022-04-23 21:54:01 -0400

    Agregadas animacion de entrada (on scroll)

--- js/simple.js
+++ js/simple.js
@@ -1,4 +1,17 @@
 const menuList = document.getElementById('menu')
-const menu = () => { 
+const menu = () => {
     menuList.style.display === 'none' || menuList.style.display === '' ? menuList.style.display = 'block' : menuList.style.display = 'none';
 }
+
+if (!!window.IntersectionObserver) {
+    let observer = new IntersectionObserver((divs, observer) => {
+        divs.forEach(div => {
+            if (div.isIntersecting) {
+                div.target.classList.add("animacionEntrada")
+                observer.unobserve(div.target);
+            }
+        });
+    }, { rootMargin: "0px 0px -150px 0px" });
+    document.querySelectorAll('.preAnimacion').forEach(one => { observer.observe(one) });
+}
+else document.querySelectorAll('.preAnimacion').forEach(one => { one.classList.remove("preAnimacion") });;
